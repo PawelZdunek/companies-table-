a5cc5e906741918d4bb527c08eeb60a8338832fe
PawelZdunek <pawel@zdunek.org.pl>
2020-06-06 00:55:30 +0200

    Reconstruction of the entire project

--- js/main.js
+++ js/main.js
@@ -1,97 +1,271 @@
-const getData = () => {
-    axios.get('https://recruitment.hal.skygate.io/companies').then(response => {
-        const data = response.data
+let companyData = 'https://recruitment.hal.skygate.io/companies'
+let importCompanyData = axios.get(companyData)
+
+const table = [];
+
+for (let i = 1; i <= 300; i++) {
+    let one = `https://recruitment.hal.skygate.io/incomes/${i}`
+    let importOne = axios.get(one);
+    table.push(importOne)
+}
+
+// console.log(table);
+
+const tableBody = document.querySelector('.table-body')
+
+const maxNumber = 10;
 
 
-        const table = document.createElement('table');
-        table.classList.add('initial-table');
-        document.body.appendChild(table)
+const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...responses) => {
+    const importCompanyData = responses[0]
 
-        const tableHead = document.createElement('thead');
-        tableHead.classList.add('table-head');
-        table.appendChild(tableHead)
+    const data = importCompanyData.data
 
-        const tableHeadRow = document.createElement('tr');
-        tableHeadRow.classList.add('table-head-row');
-        tableHead.appendChild(tableHeadRow)
+    data.sort((a, b) => a.id -b.id)
 
-        for(let [key] of Object.entries(data[0])) {
-                
-            let tableHeadHeading = document.createElement('th');
-            tableHeadHeading.classList.add(`table-head-heading`)   
-            tableHeadHeading.innerHTML = key;
-            tableHeadRow.appendChild(tableHeadHeading)
+    data.forEach(element => {
+        element.value = responses[element.id].data.incomes
+    });
+
+    const lolo = [];
+    const holo = [];
+
+    data.forEach( element => {
+        const valium = element.value
+
+        const score = valium.reduce((acc, element) => {
+          return acc + Number(element.value)
+        }, 0)  
+        const scoreRound = score.toFixed(2);
+        element.valueAll = scoreRound;
+        
+        const tableRow = document.createElement('tr');
+        tableRow.classList.add('table-row')
+        
+        for(let [key, value] of Object.entries(element)) {
+            if (key == 'id' || key == 'name' || key == 'city' || key == 'valueAll') { 
+                let tableElement = document.createElement('td');
+                tableElement.innerHTML = value;
+                tableRow.appendChild(tableElement)
+                tableElement.classList.add(`table-td`)
+                tableElement.classList.add(`table-td-${key}`)
+            } 
+        }
+        
+        lolo.push(element)
+        holo.push(tableRow)
+    })
+    
+            let maxNumberId = maxNumber;
+            let maxNumberName = maxNumber;
+            let maxNumberCity = maxNumber;
+            let maxNumberIncome = maxNumber;
+    
+        const buttonOne = document.querySelector('.number-button-1')
+    
+        const tableHeadId = document.querySelector('.table-head-id')
+        const tableHeadName = document.querySelector('.table-head-name')
+        const tableHeadCity = document.querySelector('.table-head-city')
+        const tableHeadIncome = document.querySelector('.table-head-income')
+
+
+    function newPage() {
+        const inputNumber = this.innerHTML
+        if(tableHeadId.classList.contains('table-head-sort')) {
+            maxNumberId = inputNumber * maxNumber;
+        } else if(tableHeadName.classList.contains('table-head-sort')) {
+            maxNumberName = inputNumber * maxNumber;
+        } else if(tableHeadCity.classList.contains('table-head-sort')) {
+            maxNumberCity = inputNumber * maxNumber;
+        } else if(tableHeadIncome.classList.contains('table-head-sort')) {
+            maxNumberIncome = inputNumber * maxNumber;
+        } else {console.log('error newPage function')}
+    }
+
+    function newPageFunction() {
+        if(tableHeadId.classList.contains('table-head-sort')) {
+            rowRemove();
+            sortById();
+        } else if(tableHeadName.classList.contains('table-head-sort')) {
+            rowRemove();
+            sortByName();
+        } else if(tableHeadCity.classList.contains('table-head-sort')) {
+            rowRemove();
+            sortByCity();
+        } else if(tableHeadIncome.classList.contains('table-head-sort')) {
+            rowRemove();
+            sortByIncome();
+        } else {console.log('error newPageFunction function')}
+    }
+
+    function addColor() {
+
+        const buttons = document.querySelectorAll('.number-button')
+        buttons.forEach(e => e.classList.remove('number-button-active'))
+
+        this.classList.add('number-button-active')
+    }
+
+    const tableIndexBox = document.querySelector('.table-index-box')
+
+    for(let i = 1; i <= (300 / maxNumber); i++){
+        let numberButton = document.createElement('button');
+        numberButton.innerHTML = i;
+        tableIndexBox.appendChild(numberButton)
+        numberButton.classList.add(`number-button`)
+        numberButton.classList.add(`number-button-${i}`)
+
+        numberButton.addEventListener('click', newPage)
+        numberButton.addEventListener('click', newPageFunction)
+        numberButton.addEventListener('click', addColor)
+
+        if (i === 1){
+            numberButton.classList.add('number-button-active')  
+        }
+    }
+
+    function rowRemove() {
+        const list = document.querySelectorAll('.table-row')
+        list.forEach(row => row.remove())
+    }
+    
+    function removeClass(){
+        
+        tableHeadId.classList.remove('table-head-sort')
+        tableHeadName.classList.remove('table-head-sort')
+        tableHeadCity.classList.remove('table-head-sort')
+        tableHeadIncome.classList.remove('table-head-sort')
+        
+        this.classList.add('table-head-sort')
+        
+        const list = document.querySelectorAll('.table-row')
+        list.forEach(row => row.remove())
+
+        const buttons = document.querySelectorAll('.number-button')
+        buttons.forEach(e => e.classList.remove('number-button-active'))
+
+        const buttonOne = document.querySelector('.number-button-1')
+        buttonOne.classList.add('number-button-active')   
+    }
+    
+        tableHeadId.addEventListener('click', removeClass)
+        tableHeadName.addEventListener('click', removeClass)
+        tableHeadCity.addEventListener('click', removeClass)
+        tableHeadIncome.addEventListener('click', removeClass)
+
+    function sortById() {
+        if(this !== undefined){
+            maxNumberId = maxNumber;
+            // tableHeadId.classList.toggle('sorte-by')
         }
 
-        const tableBody = document.createElement('tbody');
-        tableBody.classList.add('table-body');
-        table.appendChild(tableBody)
-
-        const tableRowsByName = () => {
-
-            const list = document.querySelectorAll('.table-row')
-
-            list.forEach(row => row.remove())
-
-            data
-            .sort((a, b) =>{
-                return a.name > b.name? 1: -1;
-            })
-            .map(element =>{
+        holo
+        .sort((a, b) => {
+            const aValue = Number(a.querySelector('.table-td-id').innerHTML)
+            const bValue = Number(b.querySelector('.table-td-id').innerHTML)
             
-            const tableRow = document.createElement('tr');
-            tableRow.classList.toggle('table-row')
-
-            for(let [key, value] of Object.entries(element)) {
-                
-                let tableElement = document.createElement('td');
-                tableElement.innerHTML = value;
-                tableRow.appendChild(tableElement)
-                tableElement.classList.add(`table-td`)
-                tableElement.classList.add(`table-td-${key}`)   
-            }
-        
-            tableBody.appendChild(tableRow);
-        })}
-
-        const sortingButton = document.createElement('button');
-        sortingButton.classList.add('sorting-button');
-        document.body.appendChild(sortingButton)
-        sortingButton.innerHTML = `Sort by Name`;
-
-        sortingButton.addEventListener('click', tableRowsByName)
-
-
-        const tableRows = data
-        .sort((a, b) =>{
-            return a.id - b.id
+            // if(tableHeadId.classList.contains('sorte-by')){
+                return aValue > bValue? 1 : -1;
+            // }else{
+            //      return aValue > bValue? -1 : 1;
+            // }
         })
-        .map(element =>{
+        .filter((element, index) => {
+            if (index < maxNumberId && index > (maxNumberId - maxNumber -1)){
+                return element;
+            }
+        })
+        .map(element => tableBody.appendChild(element))
+    }
+
+    tableHeadId.addEventListener('click', sortById)
+
+    function sortByName() {
+        if(this !== undefined){
+            maxNumberName = maxNumber;
+            // tableHeadName.classList.toggle('sorte-by')
+        }
+
+        holo
+        .sort((a, b) => {
+            const aValue = a.querySelector('.table-td-name').innerHTML
+            const bValue = b.querySelector('.table-td-name').innerHTML
             
-            const tableRow = document.createElement('tr');
-            tableRow.classList.toggle('table-row')
-
-            for(let [key, value] of Object.entries(element)) {
-                
-                let tableElement = document.createElement('td');
-                tableElement.innerHTML = value;
-                tableRow.appendChild(tableElement)
-                tableElement.classList.add(`table-td`)
-                tableElement.classList.add(`table-td-${key}`)   
-            }
-        
-            tableBody.appendChild(tableRow);
+            // if(tableHeadName.classList.contains('sorte-by')){
+                return aValue > bValue? 1 : -1;
+            // } else{
+                // return aValue > bValue? -1 : 1;
+            // }
         })
+        .filter((element, index ) => {
+            if (index < maxNumberName && index > (maxNumberName - maxNumber -1)){
+                return element;
+            }
+        })
+        .map(element => tableBody.appendChild(element))
+    }
 
-    })
-}
-getData();
+    tableHeadName.addEventListener('click', sortByName)
+
+    function sortByCity() {
+        if(this !== undefined){
+            maxNumberCity = maxNumber;
+            // tableHeadCity.classList.toggle('sorte-by')
+        }
+
+        holo
+        .sort((a, b) => {
+            const aValue = a.querySelector('.table-td-city').innerHTML
+            const bValue = b.querySelector('.table-td-city').innerHTML
+            
+            // if(tableHeadCity.classList.contains('sorte-by')){
+                return aValue > bValue? 1 : -1;
+            // } else{
+            //     return aValue > bValue? -1 : 1;
+            // }
+        })
+        .filter((element, index) => {
+            if (index < maxNumberCity && index > (maxNumberCity - maxNumber -1)){
+                return element;
+            }
+        })
+        .map(element => tableBody.appendChild(element))
+    }
+
+    tableHeadCity.addEventListener('click', sortByCity)
 
 
-const getDataIncomes = () => {
-    axios.get('https://recruitment.hal.skygate.io/incomes/:id').then(response => {
-        console.log(response);
-    })
-}
+    function sortByIncome() {
+        if(this !== undefined){
+            maxNumberIncome = maxNumber;
+            // tableHeadIncome.classList.toggle('sorte-by')
+        }
 
-getDataIncomes();
+        holo
+        .sort((a, b) => {
+            const aValue = a.querySelector('.table-td-valueAll').innerHTML
+            const bValue = b.querySelector('.table-td-valueAll').innerHTML
+            
+            // if(tableHeadIncome.classList.contains('sorte-by')){
+                // return aValue > bValue? 1 : -1;
+            // }else{
+                return aValue > bValue? -1 : 1;
+            // }
+        })
+        .filter((element, index) => {
+            if (index < maxNumberIncome && index > (maxNumberIncome - maxNumber -1)){
+                return element;
+            }
+        })
+        .map(element => tableBody.appendChild(element))
+    }
+
+    tableHeadIncome.addEventListener('click', sortByIncome)
+    sortByIncome()
+
+    // const rows = document.querySelectorAll('.table-row')
+    
+    // console.log(rows)
+
+
+}))
