b97786777dcd106c24cfc2a2f063e23615a57d4a
PawelZdunek <pawel@zdunek.org.pl>
2020-06-08 14:35:38 +0200

    Changed the way of navigating the table Make it responsive on firefox and safari

--- js/main.js
+++ js/main.js
@@ -27,8 +27,8 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
         element.value = responses[element.id].data.incomes
     });
 
-    const lolo = [];
-    const holo = [];
+    const allElementsData = [];
+    const allCreatedRows = [];
 
     data.forEach( element => {
         const valium = element.value
@@ -52,35 +52,17 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
             } 
         }
         
-        lolo.push(element)
-        holo.push(tableRow)
+        allElementsData.push(element)
+        allCreatedRows.push(tableRow)
     })
     
-            let maxNumberId = maxNumber;
-            let maxNumberName = maxNumber;
-            let maxNumberCity = maxNumber;
-            let maxNumberIncome = maxNumber;
-    
-        const buttonOne = document.querySelector('.number-button-1')
+        let maxNumberNow = 1;
     
         const tableHeadId = document.querySelector('.table-head-id')
         const tableHeadName = document.querySelector('.table-head-name')
         const tableHeadCity = document.querySelector('.table-head-city')
         const tableHeadIncome = document.querySelector('.table-head-income')
 
-
-    function newPage() {
-        const inputNumber = this.innerHTML
-        if(tableHeadId.classList.contains('table-head-sort')) {
-            maxNumberId = inputNumber * maxNumber;
-        } else if(tableHeadName.classList.contains('table-head-sort')) {
-            maxNumberName = inputNumber * maxNumber;
-        } else if(tableHeadCity.classList.contains('table-head-sort')) {
-            maxNumberCity = inputNumber * maxNumber;
-        } else if(tableHeadIncome.classList.contains('table-head-sort')) {
-            maxNumberIncome = inputNumber * maxNumber;
-        } else {console.log('error newPage function')}
-    }
 
     function newPageFunction() {
         if(tableHeadId.classList.contains('table-head-sort')) {
@@ -98,31 +80,123 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
         } else {console.log('error newPageFunction function')}
     }
 
-    function addColor() {
 
-        const buttons = document.querySelectorAll('.number-button')
-        buttons.forEach(e => e.classList.remove('number-button-active'))
-
-        this.classList.add('number-button-active')
-    }
-
-    const tableIndexBox = document.querySelector('.table-index-box')
-
-    for(let i = 1; i <= (data.length / maxNumber); i++){
-        let numberButton = document.createElement('button');
-        numberButton.innerHTML = i;
-        tableIndexBox.appendChild(numberButton)
-        numberButton.classList.add(`number-button`)
-        numberButton.classList.add(`number-button-${i}`)
-
-        numberButton.addEventListener('click', newPage)
-        numberButton.addEventListener('click', newPageFunction)
-        numberButton.addEventListener('click', addColor)
-
-        if (i === 1){
-            numberButton.classList.add('number-button-active')  
+    function addBeforeNumberPrevious() {
+        if (maxNumberNow >= 10) {
+            beforeNumberPrevious.innerHTML = maxNumberNow - 9;
+            beforeNumberPrevious.classList.remove('nav-button-non-active')
+        } else {
+            beforeNumberPrevious.innerHTML = '...';
+            beforeNumberPrevious.classList.add('nav-button-non-active')
         }
     }
+
+    function addNumberPrevious() {
+        if (maxNumberNow >= 5) {
+            numberPrevious.innerHTML = maxNumberNow - 4;
+            numberPrevious.classList.remove('nav-button-non-active')
+        } else {
+            numberPrevious.innerHTML = '...';
+            numberPrevious.classList.add('nav-button-non-active')
+        }
+    }
+
+    function addPrevious() {
+        if (maxNumberNow > 1) {
+            previous.classList.remove('nav-button-non-active')
+        } else {
+            previous.classList.add('nav-button-non-active')
+        }
+    }
+
+    function addExelent() {
+        
+        exelentNumber.innerHTML = maxNumberNow;
+    }
+
+    function addNext() {
+        if (maxNumberNow < 30) {
+            next.classList.remove('nav-button-non-active')
+        } else {
+            next.classList.add('nav-button-non-active')
+        }
+    }
+
+    function addNumberNext() {
+        if (maxNumberNow <= 26) {
+            numberNext.innerHTML = maxNumberNow + 4;
+            numberNext.classList.remove('nav-button-non-active')
+        } else {
+            numberNext.innerHTML = '...';
+            numberNext.classList.add('nav-button-non-active')
+        }
+    }
+
+    function addAfterNumberNext() {
+        if (maxNumberNow <= 21) {
+            afterNumberNext.innerHTML = maxNumberNow + 9;
+            afterNumberNext.classList.remove('nav-button-non-active')
+        } else {
+            afterNumberNext.innerHTML = '...';
+            afterNumberNext.classList.add('nav-button-non-active')
+        }
+    }
+    
+    function addNewPage() {
+        if (this === beforeNumberPrevious) {
+            if (maxNumberNow >= 10){
+                maxNumberNow = maxNumberNow - 10;
+            }
+        } else if (this === numberPrevious) {
+            if (maxNumberNow > 5){
+                maxNumberNow = maxNumberNow - 5;
+
+            }
+        } else if (this === previous) {
+            if (maxNumberNow > 1){
+                maxNumberNow = maxNumberNow - 1;
+
+            }
+        } else if (this === next) {
+            if (maxNumberNow < 30){
+                maxNumberNow = maxNumberNow + 1;
+            }
+        } else if (this === numberNext) {
+            if (maxNumberNow <= 25){
+                maxNumberNow = maxNumberNow + 5;
+            }
+        } else if (this === afterNumberNext) {
+            if (maxNumberNow <= 20){
+                maxNumberNow = maxNumberNow + 10;
+            }
+        }  
+        addBeforeNumberPrevious()
+        addNumberPrevious()
+        addPrevious()
+        addExelent()
+        addNext()
+        addNumberNext()
+        addAfterNumberNext()
+
+        newPageFunction();
+    }
+    
+    const beforeNumberPrevious = document.querySelector('.before-number-previous')
+    const numberPrevious = document.querySelector('.number-previous')
+    const previous = document.querySelector('.previous')
+    const exelentNumber = document.querySelector('.exelent-number')
+    const next = document.querySelector('.next')
+    const numberNext = document.querySelector('.number-next')
+    const afterNumberNext = document.querySelector('.after-number-next')
+    
+    previous.addEventListener('click', addNewPage)
+    next.addEventListener('click', addNewPage)
+    numberPrevious.addEventListener('click', addNewPage)
+    numberNext.addEventListener('click', addNewPage)
+    afterNumberNext.addEventListener('click', addNewPage)
+    beforeNumberPrevious.addEventListener('click', addNewPage)
+
+    
 
     function rowRemove() {
         const list = document.querySelectorAll('.table-row')
@@ -155,7 +229,7 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
 
     function checkDate() {
         const contentId = document.querySelector('.content-id-p').innerHTML
-        const goodTable = lolo.find(e => e.id == contentId);
+        const goodTable = allElementsData.find(e => e.id == contentId);
 
         const startDateValue = document.querySelector('#start-date').value
         const endDateValue = document.querySelector('#end-date').value
@@ -196,15 +270,11 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
     endDate.addEventListener('change', checkDate)
 
 
-    const nowData = new Date();
-    const nowYear = nowData.getFullYear();
-    const nowMounth = nowData.getMonth() + 1;
-
     function findData() {
         
         const thisId = this.querySelector('.table-td-id').innerHTML
         
-        const finalObject = lolo.find(e => e.id == thisId)
+        const finalObject = allElementsData.find(e => e.id == thisId)
 
         const boxId = document.querySelector('.content-id-p')
         boxId.innerHTML = finalObject.id
@@ -235,8 +305,7 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
             const dateTable = e.date.split('-') 
             const year = Number(dateTable[0])
             const mounth = Number(dateTable[1])
-            return mounth === 1 && year === nowYear;
-            // return mounth === (nowMounth - 1) && year === nowYear;
+            return mounth === 12 && year === 2019;
         })    
         
 
@@ -259,27 +328,26 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
         buttonClosecontent.addEventListener('click' ,function() {
             containerContent.classList.remove('container-content-min');
         })
+
+        checkDate()
     }
     
     function sortById() {
         if(this !== undefined){
-            maxNumberId = maxNumber;
-            // tableHeadId.classList.toggle('sorte-by')
+            maxNumberNow = 1;
+            addNewPage();
         }
 
-        const resultOfSort = holo
+        const resultOfSort = allCreatedRows
         .sort((a, b) => {
             const aValue = Number(a.querySelector('.table-td-id').innerHTML)
             const bValue = Number(b.querySelector('.table-td-id').innerHTML)
             
-            // if(tableHeadId.classList.contains('sorte-by')){
-                return aValue > bValue? 1 : -1;
-            // }else{
-            //      return aValue > bValue? -1 : 1;
-            // }
+            return aValue > bValue? 1 : -1;
+
         })
         .filter((element, index) => {
-            if (index < maxNumberId && index > (maxNumberId - maxNumber -1)){
+            if (index < (maxNumberNow * 10) && index > ((maxNumberNow * 10) - maxNumber -1)){
                 return element;
             }
         })
@@ -294,23 +362,20 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
 
     function sortByName() {
         if(this !== undefined){
-            maxNumberName = maxNumber;
-            // tableHeadName.classList.toggle('sorte-by')
+            maxNumberNow = 1;
+            addNewPage()
         }
 
-        const resultOfSort = holo
+        const resultOfSort = allCreatedRows
         .sort((a, b) => {
             const aValue = a.querySelector('.table-td-name').innerHTML
             const bValue = b.querySelector('.table-td-name').innerHTML
-            
-            // if(tableHeadName.classList.contains('sorte-by')){
-                return aValue > bValue? 1 : -1;
-            // } else{
-                // return aValue > bValue? -1 : 1;
-            // }
+
+            return aValue > bValue? 1 : -1;
+
         })
         .filter((element, index ) => {
-            if (index < maxNumberName && index > (maxNumberName - maxNumber -1)){
+            if (index < (maxNumberNow * 10) && index > ((maxNumberNow * 10) - maxNumber -1)){
                 return element;
             }
         })
@@ -325,23 +390,19 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
 
     function sortByCity() {
         if(this !== undefined){
-            maxNumberCity = maxNumber;
-            // tableHeadCity.classList.toggle('sorte-by')
+            maxNumberNow = 1;
+            addNewPage()
         }
 
-        const resultOfSort = holo
+        const resultOfSort = allCreatedRows
         .sort((a, b) => {
             const aValue = a.querySelector('.table-td-city').innerHTML
             const bValue = b.querySelector('.table-td-city').innerHTML
             
-            // if(tableHeadCity.classList.contains('sorte-by')){
-                return aValue > bValue? 1 : -1;
-            // } else{
-            //     return aValue > bValue? -1 : 1;
-            // }
+            return aValue > bValue? 1 : -1;
         })
         .filter((element, index) => {
-            if (index < maxNumberCity && index > (maxNumberCity - maxNumber -1)){
+            if (index < (maxNumberNow * 10) && index > ((maxNumberNow * 10) - maxNumber -1)){
                 return element;
             }
         })
@@ -355,25 +416,21 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
     tableHeadCity.addEventListener('click', sortByCity)
 
 
-    function sortByIncome(start) {
+    function sortByIncome() {
         if(this !== undefined){
-            maxNumberIncome = maxNumber;
-            // tableHeadIncome.classList.toggle('sorte-by')
+            maxNumberNow = 1;
+            addNewPage()
         }
 
-        const resultOfSort = holo
+        const resultOfSort = allCreatedRows
         .sort((a, b) => {
             const aValue = a.querySelector('.table-td-valueAll').innerHTML
             const bValue = b.querySelector('.table-td-valueAll').innerHTML
-            
-            // if(tableHeadIncome.classList.contains('sorte-by')){
-                // return aValue > bValue? 1 : -1;
-            // }else{
-                return aValue > bValue? -1 : 1;
-            // }
+
+            return aValue > bValue? -1 : 1;
         })
         .filter((element, index) => {
-            if (index < maxNumberIncome && index > (maxNumberIncome - maxNumber -1)){
+            if (index < (maxNumberNow * 10) && index > ((maxNumberNow * 10) - maxNumber -1)){
                 return element;
             }
         })
@@ -390,12 +447,5 @@ const tableTwo = axios.all([importCompanyData, ...table]).then(axios.spread((...
 
     const loadViev = document.querySelector('.container-load');
     loadViev.classList.add('container-load-end')
-
-    // const rows = document.querySelectorAll('.table-row')
-    
-    // console.log(rows)
-
-    // console.log(lolo)
-    // console.log(holo)
 
 }))
